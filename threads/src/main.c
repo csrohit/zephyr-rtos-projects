0e807453075aa12810f15c0e5de74213102748e2
Rohit Nimkar <nehalnimkar@gmail.com>
2022-08-01 21:05:36 +0530

    ✨ add multiple threads

--- threads/src/main.c
+++ threads/src/main.c
@@ -9,22 +9,70 @@
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/drivers/uart.h>
 
+/* size of stack area used by each thread */
+#define STACKSIZE 128
+
+void threadA(void *dummy1, void *dummy2, void *dummy3);
+void threadB(void *dummy1, void *dummy2, void *dummy3);
+
+K_THREAD_STACK_DEFINE(threadA_stack_area, STACKSIZE);
+static struct k_thread threadA_data;
+
+K_THREAD_STACK_DEFINE(threadB_stack_area, STACKSIZE);
+static struct k_thread threadB_data;
 
 void main(void)
 {
-const struct device *dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+	const struct device *dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 	uint32_t dtr = 0;
 
-	if (usb_enable(NULL)) {
+	if (usb_enable(NULL))
+	{
 		return;
 	}
 
 	/* Poll if the DTR flag was set */
-	while (!dtr) {
+	while (!dtr)
+	{
 		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
 		/* Give CPU resources to low priority threads. */
 		k_sleep(K_MSEC(100));
 	}
 
-    printk("Multi thread example\n");
+	printk("Multi thread example\n");
+
+	k_thread_create(&threadA_data, threadA_stack_area, K_THREAD_STACK_SIZEOF(threadA_stack_area), threadA, NULL, NULL, NULL, 7, 0, K_FOREVER);
+	k_thread_name_set(&threadA_data, "thread_a");
+
+	k_thread_create(&threadB_data, threadB_stack_area, K_THREAD_STACK_SIZEOF(threadB_stack_area), threadB, NULL, NULL, NULL, 7, 0, K_FOREVER);
+	k_thread_name_set(&threadB_data, "thread_b");
+
+	k_thread_start(&threadA_data);
+	k_thread_start(&threadB_data);
+}
+
+void threadA(void *dummy1, void *dummy2, void *dummy3)
+{
+	ARG_UNUSED(dummy1);
+	ARG_UNUSED(dummy2);
+	ARG_UNUSED(dummy3);
+
+	while (1)
+	{
+		printk("Thread A\n");
+		k_msleep(1000);
+	}
+}
+
+void threadB(void *dummy1, void *dummy2, void *dummy3)
+{
+	ARG_UNUSED(dummy1);
+	ARG_UNUSED(dummy2);
+	ARG_UNUSED(dummy3);
+
+	while (1)
+	{
+		printk("Thread B\n");
+		k_msleep(1000);
+	}
 }
